dda07158d7d0aca12c116e91e9f87aaed60c3dc0
Uanderson Costa <uandersonferreiracosta@gmail.com>
2026-02-05 01:25:54 -0300

    feat: add product repository

---
+++ src/main/java/projedata/autoflex/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package projedata.autoflex.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import projedata.autoflex.entity.ProductEntity;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<ProductEntity> {
+}
