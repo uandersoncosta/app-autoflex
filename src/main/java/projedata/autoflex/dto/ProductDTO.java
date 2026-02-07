55df88098e9ab50291a7fb3c1b0ef02db1d77665
Uanderson Costa <uandersonferreiracosta@gmail.com>
2026-02-07 19:19:07 -0300

    feat: add anotation swagger

--- src/main/java/projedata/autoflex/dto/ProductDTO.java
+++ src/main/java/projedata/autoflex/dto/ProductDTO.java
@@ -2,15 +2,21 @@ package projedata.autoflex.dto;
 
 import java.math.BigDecimal;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public class ProductDTO {
+  @SuppressWarnings("deprecation")
   @NotBlank(message = "Nome é obrigatório")
+  @Schema(description = "Nome do produto", example = "Cadeira", required = true)
   public String name;
 
+  @SuppressWarnings("deprecation")
   @NotNull(message = "Preço é obrigatório")
   @Positive(message = "O preço deve ser positivo")
+  @Schema(description = "Nome do produto", example = "50.90", required = true)
   public BigDecimal price;
 }
