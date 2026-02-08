bf56bb77b356a3025e45e18858848b246ef01924
Uanderson Costa <uandersonferreiracosta@gmail.com>
2026-02-07 23:45:03 -0300

    fix: improving entity

--- src/main/java/projedata/autoflex/entity/ProductEntity.java
+++ src/main/java/projedata/autoflex/entity/ProductEntity.java
@@ -8,13 +8,37 @@ import java.util.UUID;
 @Table(name = "products")
 public class ProductEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    public UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    @Column(nullable = false)
-    public String name;
+  @Column(nullable = false)
+  private String name;
 
-    @Column(nullable = false, precision = 10, scale = 2)
-    public BigDecimal price;
+  @Column(nullable = false, precision = 10, scale = 2)
+  private BigDecimal price;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 }
